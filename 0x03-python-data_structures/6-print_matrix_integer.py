7bad69bd2e1bce04f3007335ec434aed3175f838
shamu250 <muhozashamu@gmail.com>
2023-12-01 08:38:33 +0200

    Task6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for x in range(len(i)):
+            if x < len(i) - 1:
+                print("{:d}".format(i[x]), end=" ")
+            else:
+                print("{:d}".format(i[x]))
+    if matrix == [[]]:
+        print()
