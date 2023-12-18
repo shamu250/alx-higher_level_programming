71804b2bf0f9535b734e6a50a54c48e7ea9a2f66
shamu250 <muhozashamu@gmail.com>
2023-12-18 15:03:25 +0200

    Task001

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
     except:
         return False
-    else:
-        return True
