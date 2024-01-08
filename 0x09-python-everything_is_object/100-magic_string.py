6de889a55395415acd4ec4e1490b949016e5afd2
shamu250 <muhozashamu@gmail.com>
2024-01-08 10:03:34 +0200

    Task100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def magic_string():
     return ", ".join(["BestSchool"] * magic_string.counter) if hasattr(magic_string, 'counter') else "BestSchool"
+
 magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+
