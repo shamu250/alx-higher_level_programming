a65bdb231449ce87c9d38b50c53a0c8634a9a240
shamu250 <muhozashamu@gmail.com>
2024-01-09 14:29:27 +0200

    Task02

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+''' module: 2-is_same_class
+'''
+
+
 def is_same_class(obj, a_class):
+    '''function: is_same_class
+    obj: an object
+    a_class: a class
+    returns True or False depending on if obj is of type a)class,
+    and not a subclass of a_class
+    '''
     return type(obj) == a_class
