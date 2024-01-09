0789a2b683a1be72d759a76d6c11a8977bd2e7a2
shamu250 <muhozashamu@gmail.com>
2024-01-09 14:30:36 +0200

    Task03

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+''' module: 2-is_same_class
+'''
+
+
 def is_kind_of_class(obj, a_class):
+    '''function: is_kind_of_class
+    obj: an object
+    a_class: a class
+    returns True or False depending on if obj is instance of a_class,
+    or is a subclass of a_class
+    '''
     return isinstance(obj, a_class)
