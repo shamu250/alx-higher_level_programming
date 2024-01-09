3479452bac2dc2ce6c96953b75883f24036fc73c
shamu250 <muhozashamu@gmail.com>
2024-01-09 13:45:29 +0200

    Task01

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+''' module 3-write_file
+'''
+
+
 def write_file(filename="", text=""):
-    with open(filename, mode="w", encoding="utf-8") as fd:
-        fd.write(text)
-    return len(text)
+    '''function: write_file
+    accepts filename and text string to write to file
+    '''
+    if filename == "" or type(filename) != str:
+        return
+    if type(text) != str:
+        return
+    with open(filename, 'w') as f:
+        nb_chars = f.write(text)
+        f.close()
+    return nb_chars
