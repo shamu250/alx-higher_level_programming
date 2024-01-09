5c3a7564f641589823666bf254521ef4564d1751
shamu250 <muhozashamu@gmail.com>
2024-01-09 14:31:34 +0200

    Task04

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+''' module: 4-inherits_from
+'''
+
+
 def inherits_from(obj, a_class):
-    if (type(obj) != a_class):
-        return issubclass(type(obj), a_class)
-    return False
+    '''function inherits_from
+    obj: an object
+    a_class: a class
+    returns True if type of obj inherits from a_class, but is not class
+    '''
+    return type(obj) != a_class and isinstance(obj, a_class)
