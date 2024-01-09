7f30c9a111f46c4e57ebbc708a5ed5326d33221b
shamu250 <muhozashamu@gmail.com>
2024-01-09 14:21:03 +0200

    Task00

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
-#!/usr/bin/python3
+#!/bin/python3
+''' module: 0-lookup
+'''
+
+
 def lookup(obj):
+    ''' function: lookup()
+    obj: an object
+    returns a list of attribute of "obj"
+    '''
     return dir(obj)
