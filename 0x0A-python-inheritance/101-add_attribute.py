badb04670e7aaef4c2521fcc69f156de7c17d911
shamu250 <muhozashamu@gmail.com>
2024-01-09 14:45:20 +0200

    Task101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
-def add_attribute(obj, a, v):
-    res = getattr(obj, "__doc__", None)
-    if res is None:
-        setattr(obj, a, v)
+
+"""
+Module: attribute_manager
+Description: This module provides a function for adding attributes to objects
+             while enforcing a restriction on objects with an existing docstring.
+"""
+
+def add_attribute(obj, attribute_name, value):
+    """
+    Function: add_attribute
+    Description: Adds a new attribute to the given object if it doesn't have a docstring.
+                 Raises a TypeError if the object already has a docstring.
+    Parameters:
+        - obj: The object to which the attribute should be added.
+        - attribute_name: The name of the attribute to be added.
+        - value: The value of the attribute to be added.
+    Raises:
+        - TypeError: If the object already has a docstring.
+    """
+    existing_docstring = getattr(obj, "__doc__", None)
+    if existing_docstring is None:
+        setattr(obj, attribute_name, value)
     else:
-        raise TypeError("can't add new attribute")
+        raise TypeError("Can't add new attribute to objects with an existing docstring")
