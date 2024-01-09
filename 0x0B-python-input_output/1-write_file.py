ee7c5adbbe4b87a3f0a6c4c1b2bb44c525e5662a
shamu250 <muhozashamu@gmail.com>
2024-01-09 13:43:01 +0200

    Task01

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
-    # Open the file in write mode, creating it if it doesn't exist
-    with open(filename, "w", encoding="utf-8") as file:
-        # Write the text to the file
-        file.write(text)
-        # Return the number of characters written
-        return len(text)
-
-# Example usage:
-filename = "example.txt"
-text_to_write = "Hello, this is a sample text."
-
-characters_written = write_file(filename, text_to_write)
-print(f"Number of characters written: {characters_written}")
+    with open(filename, mode="w", encoding="utf-8") as fd:
+        fd.write(text)
+    return len(text)
