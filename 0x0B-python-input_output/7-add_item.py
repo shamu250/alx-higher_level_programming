7a186cb420dd2cb14269fd8f9af5a441a4b62fb5
shamu250 <muhozashamu@gmail.com>
2024-01-09 14:16:06 +0200

    Task07

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-''' module: 9-add_item
+''' module: 7-add_item
 '''
 import json
 import sys
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 try:
